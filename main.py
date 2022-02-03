68652f7ea73c08f757d9e1cd8b55979062d4dab1
AeDani <daniel.aeberhard@gmail.com>
2022-02-03 10:31:54 +0100

    method to importe csv profiles implemented

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from profile import Profile
 import matplotlib.pyplot as plt
 
+evu25 = Profile.import_csv('Assets\evu-25.csv')
+print(evu25.df_profile.head())
 
-myProfile = Profile.random_profile('2021-01-01 00:00', '2021-12-31 23:00')
-print(len(myProfile.values))
-myProfile.values.plot()
-plt.show()
+
--- profile.py
+++ profile.py
@@ -3,19 +3,33 @@ import numpy as np
 
 
 class Profile:
-    @staticmethod
-    def random_profile(start_datetime, end_datetime):
-        date_index = Profile.create_timestamps(start_datetime, end_datetime)
-        values = pd.Series(np.random.randint(low=0, high=500, size=len(date_index)))
-        return Profile(date_index[0], values)
+    """Profile class stores any type of profile in hourly frequency"""
 
     @staticmethod
-    def create_timestamps(start_datetime, end_datetime):
+    def create_timestamps(start_datetime:str, end_datetime:str):
+        """From start to end the func returns a pandas DateTimeIndex in hourly frequency localized in Switzerland.
+        datetime as 'YYYY-MM-DD HH:MM' """
         return pd.date_range(start=start_datetime, end=end_datetime, freq='H', tz='Europe/Zurich')
 
-    def __init__(self, start_datetime, values: pd.Series):
-        self.start_datetime = start_datetime
-        self.values = values
+    @staticmethod
+    def import_csv(file, sep=';'):
+        """Imports a profile from csv file of type timestamp(hourly), values - no header - this returns a profile object"""
+        # import the data from csv
+        profile = pd.read_csv(file, sep=sep, header=None, names=['ts','mw'])
 
+        # generate a datetime index based on the timestamp from csv
+        start = profile.iloc[0][0]
+        end = profile.iloc[-1][0]
+        dtix = Profile.create_timestamps(start, end)
+
+        # return profile object
+        profile.drop(columns=['ts'], inplace=True)
+        profile['ts'] = dtix
+        profile.set_index('ts', inplace=True, drop=True)
+        return Profile(profile)
+
+
+    def __init__(self, profile: pd.DataFrame):
+        self.df_profile = profile
 
 
